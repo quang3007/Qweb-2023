a6570ad0bca9dedd3712ab27bbd1e3dea7ac316d
nguyen hong quang <nguyenhongquang30072001@gmail.com>
2023-11-15 00:10:02 +0700

    đẩy dữ liệu từ cảm biến lên web, xóa dulieu.html   guidulieu.html

--- myjavascript.js
+++ myjavascript.js
@@ -4,3 +4,16 @@ var CM1_12;
 var CM1_34;
 var CM3_12;
 var CM3_34;
+
+// Hàm chức năng đọc giá trị tag
+function IOField(ObjectID, tag) {
+    url = "io.html";
+    $.getJSON(url, function (result) {
+        document.getElementById(ObjectID).value = result[tag];
+    });
+}
+
+// HIỂN THỊ DỮ LIỆU LÊN IO FIELD
+setInterval(function () {
+    IOField("quang", "MUC_NUOC"); 
+}, 1000);
