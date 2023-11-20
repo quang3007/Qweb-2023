60d050faa45a831893a34de0dbbca4f935f8a693
nguyen hong quang <nguyenhongquang30072001@gmail.com>
2023-11-20 23:38:41 +0700

    thêm đọc data xuất report

--- myjavascript.js
+++ myjavascript.js
@@ -1,9 +1,9 @@
-var START;
-var STOP; 
-var CM1_12;
-var CM1_34;
-var CM3_12;
-var CM3_34;
+let START;
+let STOP; 
+let CM1_12;
+let CM1_34;
+let CM3_12;
+let CM3_34;
 
 let startPrj = localStorage.getItem("startProject") ? localStorage.getItem("startProject") : 0;
 let onAutoPrj = localStorage.getItem("onAuto") ? localStorage.getItem("onAuto") : 0;
@@ -13,38 +13,56 @@ let onHandmadePrj = localStorage.getItem("onHandmade") ? localStorage.getItem("o
 let onHandmadeBom1Prj = localStorage.getItem("onHandmadeBom1") ? localStorage.getItem("onHandmadeBom1") : 0;
 let onHandmadeBom2Prj = localStorage.getItem("onHandmadeBom2") ? localStorage.getItem("onHandmadeBom2") : 0;
 
-
-
 $(document).ready(function() {
     // // doi mau nut start khi bat chuong trinh 
     if (startPrj == 1) {
         $("#btn_start").css("background-color", "green");
     }
 
-    //  // doi mau nut stop khi tat chuong trinh 
-    // if (startPrj == 0) {
-    //     $("#btn_stop").css("background-color", "red");
-    // }
+     // doi mau nut stop khi tat chuong trinh 
+    if (startPrj == 0) {
+        $("#btn_stop").css("background-color", "red");
+    }
 
-    // // doi mau nut auto 
-    // if (startPrj == 1 && onAutoPrj == 1) {
-    //     $("#btn_auto").css("background-color", "green");
-    // }
+    // doi mau nut auto 
+    if (startPrj == 1 && onAutoPrj == 1) {
+        $("#btn_auto").css("background-color", "green");
+    }
 
-    // // doi mau nut auto bom 1
-    // if (startPrj == 1 && onAutoPrj == 1 && onAutoBom1Prj == 1) {
-    //     $("#btn_auto_bom1").css("background-color", "green");
-    // }
+    // doi mau nut auto bom 1
+    if (startPrj == 1 && onAutoPrj == 1 && onAutoBom1Prj == 1) {
+        $("#btn_auto_bom1").css("background-color", "green");
+    }
 
-    // // doi mau nut auto bom 2
-    // if (startPrj == 1 && onAutoPrj == 1 && onAutoBom2Prj == 1) {
-    //     $("#btn_auto_bom2").css("background-color", "green");
-    // }
+    // doi mau nut auto bom 2
+    if (startPrj == 1 && onAutoPrj == 1 && onAutoBom2Prj == 1) {
+        $("#btn_auto_bom2").css("background-color", "green");
+    }
 
-    // // doi mau nut che do tay 
-    // if (startPrj == 1 && onHandmadePrj == 1) {
-    //     $("#btn_tay").css("background-color", "green");
-    // }
+    // doi mau nut che do tay 
+    if (startPrj == 1 && onHandmadePrj == 1) {
+        $("#btn_tay").css("background-color", "green");
+    }
+
+    //doi mau che che do bat tay bom 1
+    if (startPrj == 1 && onHandmadePrj == 1 && onHandmadeBom1Prj == 1) {
+        $("#btn_tay_bat_bom1").css("background-color", "green");
+    }
+    
+    //doi mau che che do tat tay bom 1
+    if (startPrj == 1 && onHandmadePrj == 1 && onHandmadeBom1Prj == 0) {
+        $("#btn_tay_tat_bom1").css("background-color", "green");
+    }
+
+    //doi mau che che do bat tay bom 2
+    if (startPrj == 1 && onHandmadePrj == 1 && onHandmadeBom2Prj == 1) {
+        $("#btn_tay_bat_bom2").css("background-color", "green");
+    }
+
+    //doi mau che che do tat tay bom 2
+    if (startPrj == 1 && onHandmadePrj == 1 && onHandmadeBom2Prj == 0) {
+        $("#btn_tay_tat_bom2").css("background-color", "green");
+    }
 });
 
 // Hàm chức năng đọc giá trị tag
@@ -94,17 +112,17 @@ function onHandmade() {
 }
 
 function onHandmadeBom1() {
-    
+    localStorage.setItem("onHandmadeBom1", 1);
 }
 
 function offHandmadeBom1() {
-    
+    localStorage.setItem("onHandmadeBom1", 0);
 }
 
 function onHandmadeBom2() {
-    
+    localStorage.setItem("onHandmadeBom2", 1);
 }
 
 function offHandmadeBom2() {
-    
+    localStorage.setItem("onHandmadeBom2", 0);
 }
--- trend.js
+++ trend.js
@@ -1,3 +1,11 @@
+let thoigian;
+let startPrj = localStorage.getItem("startProject") ? localStorage.getItem("startProject") : 0;
+let onAutoPrj = localStorage.getItem("onAuto") ? localStorage.getItem("onAuto") : 0;
+let onAutoBom1Prj = localStorage.getItem("onAutoBom1") ? localStorage.getItem("onAutoBom1") : 0;
+let onAutoBom2Prj = localStorage.getItem("onAutoBom2") ? localStorage.getItem("onAutoBom2") : 0;
+let onHandmadePrj = localStorage.getItem("onHandmade") ? localStorage.getItem("onHandmade") : 0;
+let onHandmadeBom1Prj = localStorage.getItem("onHandmadeBom1") ? localStorage.getItem("onHandmadeBom1") : 0;
+let onHandmadeBom2Prj = localStorage.getItem("onHandmadeBom2") ? localStorage.getItem("onHandmadeBom2") : 0;
 // Hàm chức năng đọc giá trị tag
 function IOField(ObjectID, tag) {
   url = "io.html";
@@ -9,11 +17,77 @@ function IOField(ObjectID, tag) {
 // HIỂN THỊ DỮ LIỆU LÊN IO FIELD
 setInterval(function () {
   IOField("quang", "MUC_NUOC");
+  IOField("hai", "START");
 }, 1000);
 
 //Ket thuc khai bao bien toan cuc
 function load_xong() {
   vedothi();
+  gettime();
+  report_display();
+}
+
+function report_display() {
+  var text_1 = document.getElementById("report1");
+  setInterval(function () {
+    let mucNuoc = $('#quang').val() ? parseFloat($('#quang').val()) : 0;
+    if (startPrj == 1) {
+      let status = "ON";
+      if ((onAutoPrj == 1 && onAutoBom1Prj == 1) || (onHandmadePrj == 1 && onHandmadeBom1Prj == 1)) {
+        statusBom1 = "ON";
+      } else {
+        statusBom1 = "OFF";
+      }
+      if ((onAutoPrj == 1 && onAutoBom2Prj == 1) || (onHandmadePrj == 1 && onHandmadeBom2Prj == 1)){
+        statusBom2 ="ON";
+      } else {
+        statusBom2 = "OFF";
+      }
+      arr_display_1 = 'Thời gian: ' + thoigian + ", Trạng thái:" + status + ", Trạng thái cửa xả 1:" + statusBom1 + ",Trạng thái cửa xả 2:" + statusBom2 + " " + "Mực nước: " + mucNuoc;
+    } else {
+      let status = "OFF";
+      arr_display_1 = 'Thời gian: ' + thoigian + ", Trạng thái:" + status + ", " + "Mực nước: " + mucNuoc;
+    }
+    var textnode_1 = document.createTextNode(arr_display_1);
+    var lb_1 = document.createElement("br");
+    text_1.appendChild(textnode_1);
+    text_1.appendChild(lb_1);
+  }, 1000);
+}
+
+function saveTextAsFile() {
+  var textToWrite = $('#report1').html();
+  var textToWrite = textToWrite.replaceAll('<br>', '\n');
+  var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+  var fileNameToSaveAs = 'REPORT-SCADA_' + thoigian;
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL !== null) {
+    // Chrome allows the link to be clicked
+    // without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM
+    // before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+  downloadLink.click();
+}
+function destroyClickedElement(event) {
+  document.body.removeChild(event.target);
+}
+
+//Lay thoi gian thuc
+function gettime() {
+  setInterval(function () {
+    d = new Date();
+    n = d.toLocaleString();
+    thoigian = n;
+  }, 1000);
 }
 
 //====================================================================================================
@@ -32,7 +106,7 @@ function vedothi() {
       {
         strokeStyle: 'rgba(119, 119, 119, 1)',
         fillStyle: 'rgba(0, 0, 0,1)', lineWidth: 1,
-        millisPerLine: 1000, verticalSections: 6
+        millisPerLine: 1000, verticalSections: 10
       },
       maxValue: 50,
       minValue: 0,
